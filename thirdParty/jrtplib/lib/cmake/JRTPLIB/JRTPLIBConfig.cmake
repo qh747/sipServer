2548734df6cd6f71b3eca0d5669094b284e2a2fb
quhan <2217362869@qq.com>
2025-04-06 16:41:48 +0000

    2025.04.07 00:40:00 MOD FUNC: modify third party dependency jthread and jrtplib.

---
+++ thirdParty/jrtplib/lib/cmake/JRTPLIB/JRTPLIBConfig.cmake
@@ -0,0 +1,7 @@
+
+set(JRTPLIB_FOUND 1)
+
+set(JRTPLIB_INCLUDE_DIRS  "/home/quhan/02_myLibrary/JThread/install/include" "/usr/local/include" "/home/quhan/02_myLibrary/JRTPLIB/install/include")
+
+set(JRTPLIB_LIBRARIES  "-L/home/quhan/02_myLibrary/JRTPLIB/install/lib" "-ljrtp" "-L/home/quhan/02_myLibrary/JThread/install/lib" "-ljthread" "/usr/local/lib/libsrtp2.a")
+
---
+++ thirdParty/jthread/lib/cmake/JThread/JThreadConfig.cmake
@@ -0,0 +1,8 @@
+
+set(JTHREAD_FOUND 1)
+
+set(JTHREAD_INCLUDE_DIRS "/home/quhan/02_myLibrary/JThread/install/include")
+
+set(JTHREAD_LIBRARIES  "-L/home/quhan/02_myLibrary/JThread/install/lib" "-ljthread")
+
+
